c6e9f74c50f396582e5910cb041feea3fe12adf1
Srijib <srijibbose@gmail.com>
2026-01-18 17:13:58 +0530

    initial commit - project setup

---
+++ src/main/kotlin/Monitoring.kt
@@ -0,0 +1,51 @@
+package com.assessment
+
+import com.codahale.metrics.*
+import com.mongodb.client.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.config.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.prometheus.*
+import java.util.concurrent.TimeUnit
+import org.slf4j.event.*
+
+fun Application.configureMonitoring() {
+    val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    
+    install(MicrometerMetrics) {
+        registry = appMicrometerRegistry
+        // ...
+    }
+    install(DropwizardMetrics) {
+        Slf4jReporter.forRegistry(registry)
+            .outputTo(this@configureMonitoring.log)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build()
+            .start(10, TimeUnit.SECONDS)
+    }
+    install(CallId) {
+        header(HttpHeaders.XRequestId)
+        verify { callId: String ->
+            callId.isNotEmpty()
+        }
+    }
+    install(CallLogging) {
+        callIdMdc("call-id")
+    }
+    routing {
+        get("/metrics-micrometer") {
+            call.respond(appMicrometerRegistry.scrape())
+        }
+    }
+}
---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,33 @@
+package com.assessment
+
+import com.codahale.metrics.*
+import com.mongodb.client.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.config.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.prometheus.*
+import java.util.concurrent.TimeUnit
+import org.slf4j.event.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
